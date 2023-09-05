fa09943a639e21b1460e5b3419f4ec20eebb77d9
modcomlearning <modcomlearning@gmail.com>
2023-09-05 12:27:03 +0300

    13th Commit

---
+++ src/styles/Global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        font-family: Arial, Helvetica, sans-serif;
+        color: yellowgreen;
+      }
+
+      p{
+        padding: 20px;
+      }
+
+    
+`
+
+export default GlobalStyle;
