517e46f760caeb81963580d2c6f0db983226d136
modcomlearning <modcomlearning@gmail.com>
2023-09-05 11:30:09 +0300

    Eleventh

---
+++ src/About.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+
+let color = "red"
+//Internal/External Styled Components
+const XH1 = styled.h1`
+    background-color: black;
+    color: ${color};
+    padding: 2px;
+`  
+
+const PrimaryInfo = styled(XH1)`
+    border-left: 5px solid orange;
+`
+
+const Section = styled.section`
+    padding: 10px;
+    background-color: gray;
+    div {
+        margin: 10px;
+        background-color: pink;
+        h1{
+             color: orange;
+             font-family: 'Courier New', Courier, monospace;
+             padding: 2px;
+        }
+                
+    }
+`
+ 
+
+const About = () => {
+    return ( 
+        <div className="about">
+            <XH1>About Us</XH1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dignissimos voluptatum, pariatur magnam voluptates veritatis dicta ex dolorem facere sit a earum aut rerum accusantium veniam et distinctio. Eos, ullam!</p>
+            <PrimaryInfo>Checkout Our Labs</PrimaryInfo>
+            <button>Back</button>
+
+            <h2>Our Mission</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas rerum magni dicta, esse, minima minus corporis laboriosam, non vitae cum numquam? Vitae distinctio, molestiae iure fugit fuga impedit aut minima?</p>
+            <div className="about-us">
+                <h3>Who we are</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ex dolorum non debitis aliquam voluptates, iure soluta? Quasi quas quibusdam dignissimos officiis deleniti harum aliquid nobis. Illum cumque voluptates hic?</p>
+            </div>
+
+            <Section>
+                <div className="form">
+                    <h1>Talk to Us</h1>
+                    <form action="">
+                        <input type="text" placeholder='Email' />
+                        <br />
+                        <button>Send Message</button>
+                     </form>
+                 </div>
+            </Section>
+    
+        </div>
+     );
+}
+ 
+export default About;
---
+++ src/styles/Secondary.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Secondary = styled.p`
+    background-color: grey;
+    color: white;
+    padding: 2px;
+    font-style: italic;
+`  
+
+export default Secondary;
+
+
+
+
